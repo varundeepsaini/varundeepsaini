c75f647ea428be789677032c7df2d4f23ef4f57d
Varun Deep Saini <varun.23bcs10048@ms.sst.scaler.com>
2024-09-17 03:29:39 +0530

    Updated The Drive Link

--- src/App.jsx
+++ src/App.jsx
@@ -408,7 +408,7 @@ function App() {
             </div>
           </div>
           <a
-            href="https://drive.google.com/file/d/your-resume-link/view"
+            href="https://drive.google.com/file/d/1WBBHgou-77qnr60rG6yL264GWVR1_h33/view"
             target="_blank"
             rel="noopener noreferrer"
             className="inline-block mt-6 px-6 py-2 bg-blue-600 text-white font-semibold rounded-lg hover:bg-blue-700 dark:bg-blue-500 dark:hover:bg-blue-600 transition-colors duration-300"
